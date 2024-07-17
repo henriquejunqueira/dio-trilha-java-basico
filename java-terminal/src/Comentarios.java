3b8410cd003f95de98fa6f7780974c52ab285754
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-07-17 12:21:21 -0300

    java doc

---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,43 @@
+public class Comentarios {
+    public static void main(String[] args){
+        // Comentário One Line
+        // Olá, eu sou um comentário em uma única linha
+
+        // Comentário Multi Line
+        /* Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhadod
+         * quando necessário
+         */
+    }
+
+    // Documentation
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public void metodo(){
+
+    }
+
+    /*
+     * Este método foi elaborado as pressas
+     * por isso eu abreviei o nome das variáveis
+     * mas olha, ele tem a finalidade de somar ou  multiplicar
+     * dois números
+     */
+
+    public int somaMultiplica (int n, int x, String m){
+        int r = 0; // r é igual ao resultado
+        if (m == "M"){ // M= multiplicação
+            r= n * x;
+        }else{
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+}
